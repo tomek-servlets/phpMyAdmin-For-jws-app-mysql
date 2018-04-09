05ab0dc655d38b0acace208cd3aab15b97552a72
tomek-servlets <tomek.servlets@gmail.com>
2018-04-09 20:01:58 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'anyRandomString'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'anyRandomStringanyRandomStringanyRandomString'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
